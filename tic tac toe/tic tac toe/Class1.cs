e4b5d572b6350d10e746a35e210da912c47792bc
Puja <pujayadav@gmail.com>
2020-10-05 14:13:35 +0530

    [puja] tic toe games

---
+++ tic tac toe/tic tac toe/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TICTacToeGame
+{
+    class class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe!");
+        }
+    }
+}
---
+++ tic tac toe/tic tac toe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace TICTacToeGame
+{
+    class TicTacToeGame
+    {
+        public char[] board = new char[10];
+
+
+        public TicTacToeGame()
+        {
+            for (int i = 0; i < 10; i++)
+                board[i] = ' ';
+        }
+    }
+}
+            
+        
+    
+
